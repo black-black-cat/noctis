637868605cd9409c3667b2cfe1fd3c0b9b83f4ee
black-black-cat <applyk99@163.com>
2021-05-05 17:14:46 +0800

    Update

--- src/createSyntax.mjs
+++ src/createSyntax.mjs
@@ -9,6 +9,9 @@ export function createSyntax(syntaxColors) {
    syntaxColors = Object.entries(syntaxColors);
    SYNTAX.map(element => {
       let name = element.name.toLowerCase();
+      if (name.indexOf('italic') > -1) {
+         element.settings.fontStyle = element.settings.fontStyle.replace(/(italic| italic)/i, '');
+      }
       syntaxColors.forEach(item => {
          if (name === item[0].toLowerCase()) {
             element.settings.foreground = item[1];
